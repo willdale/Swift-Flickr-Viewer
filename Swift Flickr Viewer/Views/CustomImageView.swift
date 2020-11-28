292bebfcaa2fd9494158e3c8e6df5ff6d57e8e5b
Will Dale <w.dale@me.com>
2020-11-28 18:27:37 +0000

    Add image cache.

--- Swift Flickr Viewer/Views/CustomImageView.swift
+++ Swift Flickr Viewer/Views/CustomImageView.swift
@@ -7,17 +7,32 @@
 
 import UIKit
 
+let imageCache = NSCache<AnyObject, AnyObject>()
+
 class CustomImageView: UIImageView {
+    
     func loadImage(using urlString: String) {
+    
         let url = URL(string: urlString)
         
+        if let imageFromCache = imageCache.object(forKey: url as AnyObject) as? UIImage {
+            self.image = imageFromCache
+            return
+        }
+
         URLSession.shared.dataTask(with: url!, completionHandler: { (data, response, error) in
             if error != nil {
-//                print("Error Loading Photo: \(error!.localizedDescription)")
                 return
             }
-            DispatchQueue.main.async {
-                self.image = UIImage(data: data!)
+            
+            if let unwrappedData = data, let imageToCache = UIImage(data: unwrappedData) {
+                DispatchQueue.main.async {
+                    self.image = imageToCache
+                }
+                DispatchQueue.global().async {
+                    imageCache.setObject(imageToCache, forKey: url as AnyObject)
+                }
+                
             }
         }).resume()
     }
@@ -41,7 +56,6 @@ class CustomProfileImageView: UIImageView {
         
         URLSession.shared.dataTask(with: url!, completionHandler: { (data, response, error) in
             if error != nil {
-//                print("Error Loading FallBack: \(error!.localizedDescription)")
                 return
             }
             DispatchQueue.main.async {
